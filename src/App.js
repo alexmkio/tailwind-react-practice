30d877de246ad3df920c21c3c187bf46e8fab218
Alex Kio <alexandermartelkio@gmail.com>
2021-09-30 21:58:14 -0400

    Show example styling for text

--- src/App.js
+++ src/App.js
@@ -3,10 +3,187 @@ import './App.css';
 function App() {
   return (
     <>
-      <header className="font-mainFont">
-        Header Using Imported Font
-      </header>
       <main>
+        <h2>
+          Fonts
+        </h2>
+
+        <section className="p-4">
+          <h3>
+            Font family
+          </h3>
+          <p>The quick brown fox - sans/default</p>
+          <p className="font-serif">The quick brown fox - font-serif</p>
+          <p className="font-mono">The quick brown fox - font-mono</p>
+          <p className="font-mainFont">The quick brown fox - imported/custom - font-mainFont</p>
+        </section>
+
+        <section className="p-4">
+          <h3>
+            Font sizing
+          </h3>
+          <p className="text-xs">The quick brown fox - text-xs</p>
+          <p className="text-sm">The quick brown fox - text-sm</p>
+          <p className="text-base">The quick brown fox - text-base</p>
+          <p className="text-lg">The quick brown fox - text-lg</p>
+          <p className="text-xl">The quick brown fox - text-xl</p>
+          <p className="text-2xl">The quick brown fox - text-2xl</p>
+          <p className="text-3xl">The quick brown fox - text-3xl</p>
+          <p className="text-4xl">The quick brown fox - text-4xl</p>
+          <p className="text-5xl">The quick brown fox - text-5xl</p>
+          <p className="text-6xl">The quick brown fox - text-6xl</p>
+          <p className="text-7xl">The quick brown fox - text-7xl</p>
+          <p className="text-8xl">The quick brown fox - text-8xl</p>
+          <p className="text-9xl">The quick brown fox - text-9xl</p>
+        </section>
+
+        <section className="p-4">
+          <h3>
+            Font weight
+          </h3>
+          <p className="font-thin">The quick brown fox - font-thin</p>
+          <p className="font-extralight">The quick brown fox - font-extralight</p>
+          <p className="font-light">The quick brown fox - font-light</p>
+          <p className="font-normal">The quick brown fox - font-normal</p>
+          <p className="font-medium">The quick brown fox - font-medium</p>
+          <p className="font-semibold">The quick brown fox - font-semibold</p>
+          <p className="font-bold">The quick brown fox - font-bold</p>
+          <p className="font-extrabold">The quick brown fox - font-extrabold</p>
+          <p className="font-black">The quick brown fox - font-black</p>
+        </section>
+
+        <section className="p-4">
+          <h3>
+            Letter Spacing
+          </h3>
+          <p className="tracking-tighter">The quick brown fox - tracking-tighter</p>
+          <p className="tracking-tight">The quick brown fox - tracking-tight</p>
+          <p className="tracking-normal">The quick brown fox - tracking-normal</p>
+          <p className="tracking-wide">The quick brown fox - tracking-wide</p>
+          <p className="tracking-wider">The quick brown fox - tracking-wider</p>
+          <p className="tracking-widest">The quick brown fox - tracking-widest</p>
+        </section>
+
+        <section className="p-4">
+          <h3>
+            Line Height
+          </h3>
+          <h4>
+            leading-none
+          </h4>
+          <p className="leading-none">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            leading-tight
+          </h4>
+          <p className="leading-tight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            leading-snug
+          </h4>
+          <p className="leading-snug">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            leading-normal
+          </h4>
+          <p className="leading-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            leading-relaxed
+          </h4>
+          <p className="leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            leading-loose
+          </h4>
+          <p className="leading-loose">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+        </section>
+
+        <section className="p-4">
+          <h3>
+            Text Align
+          </h3>
+          <h4>
+            text-left
+          </h4>
+          <p className="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            text-center
+          </h4>
+          <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            text-right
+          </h4>
+          <p className="text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            text-justify
+          </h4>
+          <p className="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+        </section>
+
+        <section className="p-4">
+          <h3>
+            Text Color
+          </h3>
+          <p className="text-purple-50">The quick brown fox - text-purple-50</p>
+          <p className="text-purple-100">The quick brown fox - text-purple-100</p>
+          <p className="text-purple-200">The quick brown fox - text-purple-200</p>
+          <p className="text-purple-300">The quick brown fox - text-purple-300</p>
+          <p className="text-purple-400">The quick brown fox - text-purple-400</p>
+          <p className="text-purple-500">The quick brown fox - text-purple-500</p>
+          <p className="text-purple-600">The quick brown fox - text-purple-600</p>
+          <p className="text-purple-700">The quick brown fox - text-purple-700</p>
+          <p className="text-purple-800">The quick brown fox - text-purple-800</p>
+          <p className="text-purple-800">The quick brown fox - text-purple-900</p>
+        </section>
+
+        <section className="p-4">
+          <h3>
+            Line Height
+          </h3>
+          <h4>
+            leading-none
+          </h4>
+          <p className="leading-none">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            leading-tight
+          </h4>
+          <p className="leading-tight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            leading-snug
+          </h4>
+          <p className="leading-snug">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            leading-normal
+          </h4>
+          <p className="leading-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            leading-relaxed
+          </h4>
+          <p className="leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+          <h4>
+            leading-loose
+          </h4>
+          <p className="leading-loose">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quia temporibus eveniet a libero incidunt suscipit laborum, rerum accusantium modi quidem, ipsam illum quis sed voluptatum quae eum fugit earum.</p>
+        </section>
+
+        <div class="text-5xl font-extrabold">
+          <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+            Hello world
+          </span>
+        </div>
+
+        <section className="p-4">
+          <h3>
+            Placeholder Text
+          </h3>
+          <input className="placeholder-blue-500 border-2" placeholder="placeholder-blue-500"></input>
+        </section>
+
+        <section className="p-4">
+          <h3>
+            Responsive
+          </h3>
+          <p className="
+            text-xs italic uppercase
+            lg:text-6xl lg:not-italic lg:lowercase
+          ">The quick brown fox</p>
+        </section>
       </main>
     </>
   );
